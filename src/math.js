ef7adf3672b121899c1a171e0845b8b98e54af85
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2021-12-07 20:49:34 +0100

    Initial commit on the branch

---
+++ src/math.js
@@ -0,0 +1,22 @@
+export default class mathOperation {
+    constructor(num1, num2) {
+      this.num1 = num1;
+      this.num2 = num2;
+    }
+  
+  sum() {
+    return (this.num1) + (this.num2);
+  }
+  
+  subtract() {
+    return (this.num1) - (this.num2);
+  }
+  
+  multiply() {
+    return (this.num1) * (this.num2);
+  }
+  
+  divide() {
+    return (this.num1) / (this.num2);
+  }
+  }
